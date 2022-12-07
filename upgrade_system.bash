3cd4d058e735c9a483a3c7765942172b1f7f4f12
Henrik Heino <henrik.heino@gmail.com>
2022-12-08 00:07:21 +0200

    Try another method to restart uWSGI

--- upgrade_system.bash
+++ upgrade_system.bash
@@ -9,10 +9,8 @@ sudo -H -u epower bash -c "cd ~epower/epower && source venv/bin/activate && ./ma
 sudo -H -u epower bash -c "cd ~epower/epower && source venv/bin/activate && ./manage.py collectstatic --noinput -v 0"
 sudo -H -u epower bash -c "cd ~epower/epower && source venv/bin/activate && ./manage.py compilemessages -v 0"
 
-# Restart uWSGI. Do an actual stop and start, because plain restart seems to keep the old code for some reason
-service uwsgi stop
-sleep 1
-service uwsgi start
+# Restart uWSGI
+/usr/sbin/service uwsgi restart
 
 # Also upgrade the script that checks updates
 cp ~epower/epower/ansible/files/check_updates.bash ~/check_updates.bash
