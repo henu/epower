d77c0a3c36b9051ece5c0f67aac1845581c89651
Henrik Heino <henrik.heino@gmail.com>
2022-12-07 23:54:25 +0200

    Log errors during the upgrade

--- ansible/files/check_updates.bash
+++ ansible/files/check_updates.bash
@@ -9,5 +9,5 @@ NEW_COMMIT=$(git log -n 1 --pretty=format:%H)
 
 # Run the upgrade script if new commits were got
 if [ "$OLD_COMMIT" != "$NEW_COMMIT" ]; then
-    bash ~epower/epower/upgrade_system.bash
+    bash ~epower/epower/upgrade_system.bash 2> ~epower/epower/logs/upgrade_errors.log
 fi
